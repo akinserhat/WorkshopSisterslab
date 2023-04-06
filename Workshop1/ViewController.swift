e818254c01d6d5368a1fa3f5a939184c45c8b860
omerkantar <iomerkantar@gmail.com>
2023-04-06 20:30:23 +0300

    Initial Commit

---
+++ Workshop1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Workshop1
+//
+//  Created by omer kantar on 6/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
